6407a0a5337a88071fbf2ae0b1a6a9685870e38f
zigzga <447587073@qq.com>
2020-03-15 22:34:49 +0800

    add any

--- app/baseTypes/any.js
+++ app/baseTypes/any.js
@@ -1,2 +1,8 @@
 "use strict";
 var a = '123';
+a = 123;
+a = true;
+a = {
+    x: '123',
+};
+var arr = [1, '123', {}, true, [123]];
--- app/baseTypes/any.ts
+++ app/baseTypes/any.ts
@@ -1,1 +1,8 @@
-let a:any = '123';
+let a: any = '123';
+a = 123;
+a = true;
+a = {
+	x: '123',
+};
+
+let arr: any[] = [1, '123', {}, true, [123]];
