a18d53c83cd37fd51c84f5f0a9b8d3fcd7b1d1ea
ironm <ironmzhang@tencent.com>
2020-03-18 21:23:00 +0800

    change reload func

--- app/function/reloadFunc.ts
+++ app/function/reloadFunc.ts
@@ -13,22 +13,22 @@
 // };
 
 // 这个时候我们会发现 ts 已经给我们报了语法的错误，但是转译成 es5 的代码的时候依然可以运行，只不过后面定义的方式会覆盖前面定义的同名函数的方法
-const reloadFunc2 = (name: string):string;
-const reloadFunc2 = (age: number):number;
-const reloadFunc2 = (age:any):any => {
-    if(typeof age === 'number') {
-        console.log(name, age)
-    }  else {
-        console.log(name)
-    }
-}
+// const reloadFunc2 = (name: string):string;
+// const reloadFunc2 = (age: number):number;
+// const reloadFunc2 = (age:any):any => {
+//     if(typeof age === 'number') {
+//         console.log(name, age)
+//     }  else {
+//         console.log(name)
+//     }
+// }
 
-function reloadFunc3(name: string):string; 
-function reloadFunc3(age: number):number;
-function reloadFunc3(str:any):any {
-    if(typeof str === 'number') {
-        console.log(name, str)
-    }  else {
-        console.log(name)
-    }
-}; 
+function reloadFunc3(name: string): string;
+function reloadFunc3(age: number): number;
+function reloadFunc3(str: any): any {
+	if (typeof str === 'number') {
+		console.log(name, str);
+	} else {
+		console.log(name);
+	}
+}
