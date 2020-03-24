fa54136777d03eca10d51a60153ad09d159dd444
ironm <ironmzhang@tencent.com>
2020-03-24 21:31:20 +0800

    get private  attr

--- app/class/class-private.ts
+++ app/class/class-private.ts
@@ -9,6 +9,5 @@ class Animal2 {
 }
 
 var a = new Animal2('private');
-console.log('---a---', a);
-a.name = '123';
-new Animal2('private').name = '432';
+a.name = '123'; // 报错，name 属性只能在 Animal2 内部使用
+new Animal2('private').name = '432'; // 报错： 属性“name”为私有属性，只能在类“Animal2”中访问。
